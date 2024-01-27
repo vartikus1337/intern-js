b072a03f1f113f6dd0cdd9c40feefb90476af61b
vartikus1337 <pasha_klevtsov@vk.com>
2024-01-27 14:47:59 +0300

    столбец цвета + показ/скрытие колонок + редактирование ячейки + чуть рефактор кода

--- solution.js
+++ solution.js
@@ -1,26 +1,39 @@
 const DATA = await fetch('data.json').then((response) => response.json());
+const TITLES = ['Имя', 'Фамилия', 'Описания', 'Цвет глаз'];
 
-let table = document.createElement('table');
-let thead = document.createElement('thead');
-let tbody = document.createElement('tbody');
+const table = document.createElement('table');
+const thead = document.createElement('thead');
+const tbody = document.createElement('tbody');
+const div = document.createElement('div');
+const textArea = document.createElement('textarea');
+const btnAccept = document.createElement('button');
+const btnCancel = document.createElement('button');
 
+btnAccept.innerText = "Change";
+btnCancel.innerText = "Cancel";
 table.style.width = "50%";
 
 document.body.appendChild(table);
-
 table.appendChild(thead)
 
 let rowHead = thead.insertRow();
-rowHead.insertCell().outerHTML = "<th>Имя</th>";
-rowHead.insertCell().outerHTML = "<th>Фамилия</th>";
-rowHead.insertCell().outerHTML = "<th>Описания</th>";
-rowHead.insertCell().outerHTML = "<th>Цвет глаз</th>";
 
-for (const td of rowHead.cells) {
-    td.classList.add("table-title");
-    td.addEventListener('click', (e) => {
-        let th = e.target;
-        sortTable(th.cellIndex);
+TITLES.forEach((title) => {
+    rowHead.insertCell().outerHTML = `<th>${title}</th>`;
+});
+
+for (const th of rowHead.cells) {
+    th.classList.add("table-title");
+    th.addEventListener('mousedown', (e) => {
+        if (e.button == 0) {
+            sortTable(e.target.cellIndex);
+        } else {
+            const cells = document.querySelectorAll(`.col-${th.cellIndex}`)
+            console.log(cells)
+            cells.forEach(cell => {
+                cell.classList.toggle('hidden');
+            });
+        }
     });
 }
 
@@ -28,28 +41,58 @@ for (const td of rowHead.cells) {
     table.appendChild(tbody)
     DATA.forEach((el) => {
         const row = tbody.insertRow();
-        row.insertCell().innerHTML = el["name"]["firstName"];
-        row.insertCell().innerHTML = el["name"]["lastName"];
-        let p = document.createElement('p');
+        // Col-1
+        let cell = row.insertCell()
+        cell.classList.add("col-0")
+        cell.innerHTML = el["name"]["firstName"];
+        // Col-2
+        cell = row.insertCell()
+        cell.classList.add("col-1")
+        cell.innerHTML = el["name"]["lastName"];
+        // Col-3
+        const p = document.createElement('p');
         p.classList.add('about-text');
         p.textContent = el["about"];
-        row.insertCell().appendChild(p)
-        row.insertCell().innerHTML = el["eyeColor"];
+        cell = row.insertCell()
+        cell.classList.add("col-2")
+        cell.appendChild(p)
+        // Col-4
+        const divColor = document.createElement('div');
+        divColor.classList.add('div-color');
+        divColor.style.backgroundColor = el["eyeColor"];
+        const pEye = document.createElement('p');
+        pEye.textContent = el["eyeColor"]
+        divColor.appendChild(pEye)
+        cell = row.insertCell()
+        cell.classList.add("col-3")
+        cell.appendChild(divColor);
     });
+    document.body.appendChild(div);
 })()
 
 table.onclick = function(e) {
-    if (e.target.tagname != 'th') return console.log(e.target.tagname);
-    let th = e.target;
-    sortTable(th.cellIndex);
+    div.appendChild(textArea);
+    textArea.style.width = 300 + 'px';
+    textArea.style.height = 200 + 'px';
+    let td = e.target
+    textArea.value = td.innerText;
+    div.appendChild(btnAccept);
+    div.appendChild(btnCancel);
+    
+    btnAccept.onclick = () => {
+         td.innerText = textArea.value;
+    }
+
+    btnCancel.onclick = () => {
+        div.innerHTML = '';
+    }
 }
 
 function sortTable(colNum) {
     let tbody = table.querySelector('tbody');
     let rowsArray = Array.from(tbody.rows);
-    let compare = function(rowA, rowB) {
+    rowsArray.sort((rowA, rowB) => {
         return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML ? 1 : -1;
-    };
-    rowsArray.sort(compare);
+    });
     tbody.append(...rowsArray);
 }
